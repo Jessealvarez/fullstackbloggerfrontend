9f17d119d9d128f9795b06f78d9c20f115b50953
Jesse Alvarez <jesse@Jesses-MBP.home>
2022-07-08 11:28:31 -0400

    missing some sorting functionality

--- fullstackbloggerfrontendold/src/Pages/Blogs.js
+++ fullstackbloggerfrontendold/src/Pages/Blogs.js
@@ -1,9 +1,111 @@
 import React from "react";
 
-const BlogsPage = ({ blogs }) => {
+const BlogsPage = ({
+  blogs,
+  sortField,
+  setSortField,
+  setSortOrder,
+  filterField,
+  setFilterField,
+  filterValue,
+  setFilterValue,
+  limit,
+  setLimit,
+  page,
+  setPage,
+}) => {
   return (
     <div className="blogs-page">
       <h1>Blogs Page</h1>
+      <label>Sort Field:</label>
+      <select>
+        <option
+          value="title"
+          onChange={(e) => {
+            const newSortField = e.target.value;
+            setSortField(newSortField);
+          }}
+        >
+          Title
+        </option>
+        <option
+          value="author"
+          onChange={(e) => {
+            const newSortField = e.target.value;
+            setSortField(newSortField);
+          }}
+        >
+          Author
+        </option>
+        <option value="created">Created At</option>
+      </select>
+      <br />
+      <label>Sort Order:</label>
+      <select>
+        <option
+          value="ASC"
+          onChange={(e) => {
+            const newSortOrder = e.target.value;
+            setSortOrder(newSortOrder);
+          }}
+        >
+          ASC
+        </option>
+        <option
+          value="DESC"
+          onChange={(e) => {
+            const newSortField = e.target.value;
+            setSortField(newSortField);
+          }}
+        >
+          DESC
+        </option>
+      </select>
+      <br />
+      <label>Filter Field:</label>
+      <select>
+        <option
+          value="filter-title"
+          onChange={(e) => {
+            const newFilterField = e.target.value;
+            setFilterField(newFilterField);
+          }}
+        >
+          Title
+        </option>
+        <option
+          value="filter-author"
+          onChange={(e) => {
+            const newFilterField = e.target.value;
+            setFilterField(newFilterField);
+          }}
+        >
+          Author
+        </option>
+      </select>
+      <br />
+      <label>Post Limit:</label>
+      <input
+        type="number"
+        //so that an int less than ONE cant be input
+        min="1"
+        value={limit}
+        onChange={(e) => {
+          const newLimit = e.target.value;
+          setLimit(newLimit);
+        }}
+      ></input>
+      <br />
+      <input
+        placeholder="Page"
+        type="number"
+        min="1"
+        value={page}
+        onChange={(e) => {
+          const newPage = e.target.value;
+          setPage(newPage);
+        }}
+      ></input>
       <div>
         {blogs.map((blog) => {
           return <BlogPost blog={blog} key={blog.id} />;
